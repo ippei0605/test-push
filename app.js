a960a06cad251d5029747cc0698e8c065e629617
Ippei SUZUKI <ippei0605@gmail.com>
2020-04-14 15:36:17 +0900

    CFにプッシュできる。

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
+'use strict';
+
 const
+  cfenv = require('cfenv'),
   express = require('express'),
   favicon = require('serve-favicon'),
+  appEnv = cfenv.getAppEnv(),
   app = express();
 
 app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use('/', express.static(__dirname + '/public'));
 
-app.listen(3000);
+app.listen(appEnv.port, () => {
+  console.log('server starting on ' + appEnv.url);
+});
